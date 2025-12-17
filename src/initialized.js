31ecd929f28a9113ebd6ee3b4ded2de8eab2b57d
Radymyr <lobodaradymyr@gmail.com>
2025-12-17 19:18:29 +0200

    feature: added ability get account info

--- src/initialized.js
+++ src/initialized.js
@@ -1,21 +1,20 @@
-import 'dotenv/config';
-import Fastify from 'fastify';
-import { Telegraf } from 'telegraf';
+import "dotenv/config";
+import Fastify from "fastify";
+import { Telegraf } from "telegraf";
 
 export const monobankToken = process.env.MONOBANK_TOKEN;
 export const baseUrl = process.env.WEB_HOOK_URL;
 export const telegramToken = process.env.TELEGRAM_TOKEN;
 export const telegramUrl = `https://api.telegram.org/bot${telegramToken}/setWebhook`;
-export const webhookRegistrationUrl =
-  'https://api.monobank.ua/personal/webhook';
 
 export const bot = new Telegraf(telegramToken);
 export const app = Fastify({ logger: true });
 
-export const monobankRoute = '/monobank/webhook';
-export const telegramRoute = '/telegram/webhook';
+export const monobankRoute = "/monobank/webhook";
+export const telegramRoute = "/telegram/webhook";
 
-export const html = `<!DOCTYPE html>
+export const html = `
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -23,13 +22,13 @@ export const html = `<!DOCTYPE html>
       name="viewport"
       content="width=device-width, initial-scale=1.0"
     />
-    <title>Document</title>
+    <title>mono-telegram-notify</title>
     <link
       rel="icon"
       type="image/x-icon"
     />
   </head>
   <body>
-    <h1>mono data</h1>
+    <h1>mono-telegram-notify</h1>
   </body>
 </html>`;
